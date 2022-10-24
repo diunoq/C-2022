bca941341e5785bd45d07fa2c8ade18187b1bd02
Karol Jodłowski <64380253+diunoq@users.noreply.github.com>
2022-10-24 17:36:42 +0200

    ver 2.3->2.4

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
+#include <cmath>
+
 #include <iostream>
 using namespace std;
 int main(){
 	
+	int n;
 	float a,b,c,d,x,x1,x2;
+	cout<<"1. Funkcja kwadratowa"<<endl;
+	cout<<"2. Funkcja liniowa"<<endl;
+	cin>>n;
+	if(n==1){
 	cout<<"Podaj a > ";
 	cin>>a;
 	cout<<"Podaj b > ";
@@ -18,10 +25,27 @@ int main(){
 	if(d==0){
 		cout<<-b/2*a;
 	}
-	if(d<0)cout<<"brak rozwi¹zañ"
+	if(d<0)cout<<"brak rozwi1zan";
 	}
 	else{
-		cout<<"funkcja liniowa"
+		cout<<"funkcja liniowa";
+	}
+	}
+	else if(n==2){
+		if(a!=0){
+		cout<<"Miejsce zerowe "<<(-b/a);
+	}
+	else{
+		if(b==0){
+			cout<<"Funkcja ma nieskonczenie wiele miejsc zerowych";
+		}
+		else{
+			cout<<"Funkcja nie ma miejsc zerowych";
+		}
+	}
+	}
+	else{
+		cout<<"wybierz 1 lub 2";
 	}
 	return 0;
 }
