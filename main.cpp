d8c6729fcfa29f70d774a23a5e9ea30ff6087fdd
Adam Nowak <kamiloles14@gmail.com>
2022-10-24 14:12:43 +0200

    wer 2.0

--- main.cpp
+++ main.cpp
@@ -2,24 +2,22 @@
 using namespace std;
 int main(){
 	
-	float a,b,c;
+	float a,b,c,d,x,x1,x2;
 	cout<<"Podaj a > ";
 	cin>>a;
 	cout<<"Podaj b > ";
 	cin>>b;
 	cout<<"Podaj c > ";
 	cin>>c;
-	if(a==0 && b==0)cout<<"0=";
-	if(a==1)cout<<"x";
-	else if(a==1)cout<<"-X";
-	else if(a!=0)cout<<a<<"x";
-	
-	if(b==1)cout<<"y=";
-	else if(b==-1)cout<<"-y=";
-	else if(b>0 && a!=0)cout<<"+"<<b<<"y=";
-	else if(b>0 && a==0)cout<<b<<"y=";
-	else ig(b<0)cout<<b<<"y=";
-	cout<<c<<endl;
+	d=(b*b)-4*a*c;
+	if(d>0){
+		cout<<"x2="<<-b-sqrt(d)/2*a;
+		cout<<"x2="<<-b+sqrt(d)/2*a;
+	}
+	if(d==0){
+		cout<<-b/2*a;
+	}
+	if(d<0)cout<<"brak rozwi¹zañ"
 	
 	return 0;
 }
