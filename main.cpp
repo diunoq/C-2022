aa1ac4987a61b3de35a405b74f50e6220c114164
Adam Nowak <kamiloles14@gmail.com>
2022-10-24 14:05:18 +0200

    wer 1.4

--- main.cpp
+++ main.cpp
@@ -2,16 +2,24 @@
 using namespace std;
 int main(){
 	
-	float a,b;
+	float a,b,c;
 	cout<<"Podaj a > ";
 	cin>>a;
 	cout<<"Podaj b > ";
 	cin>>b;
+	cout<<"Podaj c > ";
+	cin>>c;
+	if(a==0 && b==0)cout<<"0=";
+	if(a==1)cout<<"x";
+	else if(a==1)cout<<"-X";
+	else if(a!=0)cout<<a<<"x";
 	
-	if(a!=0){
-		cout<<"Miejsce zerowe "<<(-b/a);
-	}
-
+	if(b==1)cout<<"y=";
+	else if(b==-1)cout<<"-y=";
+	else if(b>0 && a!=0)cout<<"+"<<b<<"y=";
+	else if(b>0 && a==0)cout<<b<<"y=";
+	else ig(b<0)cout<<b<<"y=";
+	cout<<c<<endl;
 	
 	return 0;
 }
