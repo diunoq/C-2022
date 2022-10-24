74a78a7ceb9e63b7145344bd9986d0f610ccbc5a
Adam Nowak <kamiloles14@gmail.com>
2022-10-24 13:39:11 +0200

    v1.0

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	float a,b;
+	cout<<"Podaj a > ";
+	cin>>a;
+	cout<<"Podaj b > ";
+	cin>>b;
+	
+	if(a!=0){
+		cout<<"Miejsce zerowe "<<(-b/a);
+	}
+
+	
+	return 0;
+}
