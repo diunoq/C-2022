d576d4803e4d2f65081e6ebae59200ccf0a21094
Adam Nowak <kamiloles14@gmail.com>
2022-10-24 14:16:35 +0200

    wer 2.2

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main(){
 	cin>>b;
 	cout<<"Podaj c > ";
 	cin>>c;
+	if(a!=0){
 	d=(b*b)-4*a*c;
 	if(d>0){
 		cout<<"x2="<<-b-sqrt(d)/2*a;
@@ -18,6 +19,9 @@ int main(){
 		cout<<-b/2*a;
 	}
 	if(d<0)cout<<"brak rozwi¹zañ"
-	
+	}
+	else{
+		cout<<"funkcja liniowa"
+	}
 	return 0;
 }
